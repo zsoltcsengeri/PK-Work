34fd2eb302268813531d07db5f66f8d33d737fe3
zsoltcsengeri <zsoltcsengeri@yahoo.com>
2024-02-18 10:09:02 +0000

    dark mode applied

--- T&C/terms_dark_mode.js
+++ T&C/terms_dark_mode.js
@@ -1,4 +1,6 @@
 const switchBTN = document.querySelector(".switch-input-js")
-switchBTN.addEventListener("change", function(event){
+switchBTN.addEventListener("click", function(event){
+  document.querySelector("body").classList.toggle("dark-mode");
+  document.querySelector(".container").classList.toggle("dark-mode");
   console.log(event.target.value);
 });
